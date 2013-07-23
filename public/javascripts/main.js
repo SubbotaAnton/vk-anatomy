c554dc21999bb0a3f9ede7ded75e282019ac7552
a.subbota <subbotaanton@gmail.com>
2013-07-23 17:43:55 +0400

    second commit

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,9 +9,46 @@ $(document).ready(function(){
             name : 'Имя',
             id : 12312312
         },
+        slide2 = {
+            src : 'http://cs314624.vk.me/v314624170/1d1e/PdhQXx19554.jpg',
+            surname : 'Фамилия2',
+            name : 'Имя2',
+            id : 1231231
+        },
+        slide3 = {
+            src : 'http://cs314624.vk.me/v314624170/1d1e/PdhQXx19554.jpg',
+            surname : 'Фамилия3',
+            name : 'Имя3',
+            id : 123123
+        },
+        slide4 = {
+            src : 'http://cs314624.vk.me/v314624170/1d1e/PdhQXx19554.jpg',
+            surname : 'Фамилия4',
+            name : 'Имя4',
+            id : 12312
+        },
+        slide5 = {
+            src : 'http://cs314624.vk.me/v314624170/1d1e/PdhQXx19554.jpg',
+            surname : 'Фамилия5',
+            name : 'Имя5',
+            id : 1231
+        },
+        slide6 = {
+            src : 'http://cs314624.vk.me/v314624170/1d1e/PdhQXx19554.jpg',
+            surname : 'Фамилия6',
+            name : 'Имя6',
+            id : 123
+        },
         goodSlider = new SliderUsers(params);
 
     goodSlider.addSlide(slide);
+    goodSlider.addSlide(slide2);
+    goodSlider.addSlide(slide3);
+    goodSlider.addSlide(slide4);
+    goodSlider.addSlide(slide5);
+    goodSlider.addSlide(slide6);
     goodSlider.addSlide(slide);
+    goodSlider.addSlide(slide3);
+
 });
 
--- public/plugins/sliderUsers/slider-users.js
+++ public/plugins/sliderUsers/slider-users.js
@@ -7,7 +7,7 @@ function SliderUsers(params) {
     }
 
     this.config = {
-        wrapperClass : 'slider-user-wrapper'
+        wrapperClass : 'slider-users-wrapper'
     };
 
     this.init(params);
@@ -15,21 +15,28 @@ function SliderUsers(params) {
 
 SliderUsers.prototype = {
     data : [],
+    current : null,
+    params : {
+        count: 3 // number must be odd
+    },
     init : function (params) {
         this.initGraphic(params);
     },
     initGraphic : function (params) {
-        var $target = $(params.target);
+        var $target = $(params.target),
+            self = this;
 
         /* wrapper design */
-        $target.empty().addClass(this.config.wrapperClass);
+        $target.addClass(this.config.wrapperClass);
         if (params.width) {
             $target.css({
                 'width' : params.width
             });
         }
 
-        /* arrows design */
+        $('.' + this.config.wrapperClass + '__scroll-left', $target).on('click', function(e){self.activateSlide(self.getIdLeftSlide())});
+        $('.' + this.config.wrapperClass + '__scroll-right', $target).on('click', this.scrollRight);
+
     },
     addSlide : function (slide) {
         if (!slide.id) {
@@ -37,27 +44,69 @@ SliderUsers.prototype = {
             return null;
         }
         if (!this.isDublicateSlide(slide)) {
-            console.log('no dublicate');
             this.data.push(slide);
-        } else {
-            console.warn('dublicate');
-            this.activateSlide(slide.id);
         }
+        this.activateSlide(slide.id);
     },
     removeSlide : function (id) {
 
+    },
+    scrollLeft : function () {
+        console.log(this.getIdLeftSlide());
+        this.activateSlide(this.getIdLeftSlide());
+    },
+    scrollRight : function () {
+        console.log(this);
+        this.activateSlide(this.getIdRightSlide());
     },
     isDublicateSlide : function (slide) {
         var i;
         for (i = 0; i < this.data.length; i++) {
             if (this.data[i].id === slide.id) {
-                console.warn('Already have slide with this id');
                 return true;
             }
         }
         return false;
     },
+    getIdRightSlide : function () {
+        var number = this.current = this.data.length ? this.data.length : this.current + 1;
+        return this.data[number];
+    },
+    getIdLeftSlide : function () {
+        var number = this.current = 0 ? 0 : this.current - 1;
+        return this.data[number];
+    },
     activateSlide : function (id) {
+        var i, intStart, intEnd,
+            dataLength = this.data.length,
+            interval = Math.round((this.params.count - 1) / 2);
 
+        for (i = 0; i < dataLength; i++) {
+            if (this.data[i].id === id) {
+                intStart = i - interval > 0 ? i - interval : 0;
+                intEnd = i + interval < dataLength ? i + interval : dataLength - 1;
+                this.showSlides(intStart, intEnd);
+                this.current = i;
+            }
+        }
+    },
+    showSlides : function (start, end) {
+        var $container = $('.' + this.config.wrapperClass + '__content'),
+            self = this,
+            i, id, html;
+        $container.empty();
+
+        for (i = start; i <= end; i++) {
+            html = '<h2>' + this.data[i].surname + ' ' + this.data[i].name + '</h2>';
+            id = self.data[i].id;
+            $('<div />', {
+                'html' : html,
+                'data-id' : id,
+                click : function(e) {
+                    var id = $(e.currentTarget).data('id');
+                    self.activateSlide(id);
+                }
+            }).appendTo($container);
+        }
     }
 };
